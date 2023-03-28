6649e260d4d4840806853fcaa7affea9f8b12d06
Anuppaswan24 <119486812+Anuppaswan24@users.noreply.github.com>
2023-03-28 01:54:40 -0700

    Add files via upload

---
+++ task7/doms.js
@@ -0,0 +1,67 @@
+// teaversing the dom
+var itemList = document.querySelector('#items');
+
+// parentnode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'yellow';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'pink';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//  childnodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow'; 
+
+// firstchild
+// console.log(itemList.lastchild);
+
+// lastelementchild
+// console.log(itemList.firstElementchild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// nextsibling
+// console.log(itemList.nextSibling);
+
+// nextElementsibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// consolechange.log(itemList.previousSibling);
+
+//  previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// create a div
+//  var newDiv = document.createElement('div');
+// newDiv.className= ' hello';
+
+// // add Id
+// newDiv.id = 'hello1'
+
+// add attr
+// newDiv.setAttribute('title', 'Hello Div');
+
+// // create text node
+// var newDivText = document.createTextNode('Hello World');
+
+// // add text to div
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1'); 
+
+// console.log(newDiv);
+
+// newDiv.style.fontSize = '30px';
+
+// container.insertBefore(newDiv, h1);
+
